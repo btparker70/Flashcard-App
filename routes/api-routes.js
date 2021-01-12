764e1c982bb3ffd7b568bfad0794471e3eea7ed3
Brian Parker <btparker70@gmail.com>
2021-01-11 18:50:59 -0800

    added findall for api-routes

---
+++ routes/api-routes.js
@@ -0,0 +1,16 @@
+// This file offers a set of routes for displaying and saving data to the db
+
+// Dependencies
+// Requiring our models
+var db = require("./models");
+
+// Routes
+module.exports = function(app) {
+    // GET route for getting all the flashcards
+    app.get("/api/flashcards", function(req, res) {
+        // findAll returns all entries for a table 
+        db.Deck.findAll({}).then(function(dbDeck) {
+            res.json(dbDeck);
+        })
+    })
+};
