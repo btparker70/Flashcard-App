be4794811d6d2e11ddd449a014b6bc60343b0424
Brian Parker <btparker70@gmail.com>
2021-01-11 18:38:41 -0800

    server.js made

--- server.js
+++ server.js
@@ -1,4 +1,28 @@
+// This file is the node server. It is the initial starting point of the project
+
 // Dependencies
 var express = require("express");
-// var bodyParser = require("body-parser");
-require('dotenv').config();
+
+// Set up the express app
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Requiring our models for syncing
+var db = require("./models");
+
+// Sets up the express app to handle data parsing
+app.use(express.urlencoded({extended: true}));
+app.use(experss.json());
+
+// Static directory
+app.use(express.static("public"));
+
+// Routes
+require("./routes/api-routes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
